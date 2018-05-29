ba5bb9b245163f6cf47fc3c3c32bc00599e738dc
loganes1 <sau13007@byui.edu>
2018-05-29 16:30:38 -0600

    Worked on styling

--- Project 2/script.js
+++ Project 2/script.js
@@ -3,35 +3,52 @@ window.addEventListener('load', function() {
 });
 
 function createMovie(movie) {
-  let movieLi = document.createElement('li');
+  let li = document.createElement('li');
+  li.appendChild(createTitle(movie.Title, movie.Year));
+  let section = document.createElement('section');
+  section.className = "container";
+  section.appendChild(createImg(movie.Poster));
+  section.appendChild(createDirector(movie.Director));
+  section.appendChild(createRated(movie.Rated));
+  section.appendChild(createPlot(movie.Plot));
+  li.appendChild(section);
+  return li;
+}
 
+function createTitle(title, year) {
   let div = document.createElement('div');
-  let poster = document.createElement('img');
-  poster.src = movie.Poster;
-  div.appendChild(poster);
+  div.className = 'titleDiv';
+  let myTitle = document.createElement('h2');
+  myTitle.innerHTML = `${title} (${year})`;
+  div.appendChild(myTitle);
+  return div;
+}
 
-  let div2 = document.createElement('div');
-  let name = document.createElement('H2');
-  name.innerHTML = `${movie.Title} (${movie.Year})`;
+function createImg(poster) {
+  let myPoster = document.createElement('img');
+  myPoster.src = poster;
+  return myPoster;
+}
+
+function createDirector(str) {
   let director = document.createElement('p');
   director.className = "director";
-  director.innerHTML = `Directed by: <strong>${movie.Director}</strong>`;
+  director.innerHTML = `Directed by: <strong>${str}</strong>`;
+  return director;
+}
+
+function createRated(rated) {
   let rating = document.createElement('p');
   rating.className = "rated";
-  rating.innerHTML = movie.Rated;
-  let description = document.createElement('P');
-  description.className = "plot";
-  description.innerHTML = movie.Plot;
-  div2.appendChild(name);
-  div2.appendChild(director);
-  div2.appendChild(rating);
+  rating.innerHTML = `Rated: ${rated}`;
+  return rating;
+}
 
-  movieLi.appendChild(div);
-  movieLi.appendChild(div2);
-  movieLi.appendChild(document.createElement('br'));
-  movieLi.appendChild(description);
-
-  return movieLi;
+function createPlot(plot) {
+  let myPlot = document.createElement('P');
+  myPlot.className = "plot";
+  myPlot.innerHTML = plot;
+  return myPlot;
 }
 
 $("button").click(function(){
@@ -44,5 +61,6 @@ $("button").click(function(){
       console.dir(result);
       if (result.Error) { console.log(result.Error); return; }
       listOfMovies.appendChild(createMovie(result));
+      // listOfMovies.lastChild.innerHTML = `<h2>${result.Title}</h2>`;
   });
 });
