ef0961fa523b0ed0b6489fee8b8acb2e4942a8d4
loganes1 <sau13007@byui.edu>
2018-05-29 13:27:37 -0600

    Worked in Styling

--- Project 2/script.js
+++ Project 2/script.js
@@ -3,23 +3,35 @@ window.addEventListener('load', function() {
 });
 
 function createMovie(movie) {
-  let movieDiv = document.createElement('li');
+  let movieLi = document.createElement('li');
 
+  let div = document.createElement('div');
   let poster = document.createElement('img');
   poster.src = movie.Poster;
+  div.appendChild(poster);
+
+  let div2 = document.createElement('div');
   let name = document.createElement('H2');
-  name.innerHTML = movie.Title;
-  let rating = document.createElement('H3');
+  name.innerHTML = `${movie.Title} (${movie.Year})`;
+  let director = document.createElement('p');
+  director.className = "director";
+  director.innerHTML = `Directed by: <strong>${movie.Director}</strong>`;
+  let rating = document.createElement('p');
+  rating.className = "rated";
   rating.innerHTML = movie.Rated;
   let description = document.createElement('P');
+  description.className = "plot";
   description.innerHTML = movie.Plot;
+  div2.appendChild(name);
+  div2.appendChild(director);
+  div2.appendChild(rating);
 
-  movieDiv.appendChild(poster);
-  movieDiv.appendChild(name);
-  movieDiv.appendChild(rating);
-  movieDiv.appendChild(description);
+  movieLi.appendChild(div);
+  movieLi.appendChild(div2);
+  movieLi.appendChild(document.createElement('br'));
+  movieLi.appendChild(description);
 
-  return movieDiv;
+  return movieLi;
 }
 
 $("button").click(function(){
