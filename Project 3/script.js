6937a6dcdd63cb03c0448f7e9b917c659afaf597
loganes1 <sau13007@byui.edu>
2018-07-03 11:13:30 -0600

    Added a loading screen and started on coverting XMLHttp requests to promises

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,5 +1,10 @@
 /*jshint esversion: 6 */
 window.onload = function() {
+  const modal = document.getElementById('myModal');
+  const charCreation = document.getElementById('charCreation');
+  modal.style.display = "block";
+  charCreation.className = "blur";
+
   // mySpeciesList = JSON.parse(localStorage.getItem('speciesList'));
   // myPlanetsList = JSON.parse(localStorage.getItem('planetsList'));
   // if (!mySpeciesList) {
@@ -11,8 +16,9 @@ window.onload = function() {
   //   requestPlanets(1);
   // }
 
-  requestSpecies(1);
+  requestSpecies(1); // TODO: set as a promise
   requestPlanets(1);
+  requestVehicles(1);
 
   // console.log("The speciesList has stuff in it: ");
   // console.log(speciesList);
@@ -20,13 +26,26 @@ window.onload = function() {
   // console.log(planetsList);
   setTimeout(loadSpeciesDropdown, 3000); // since the HTTP request runs async, have this function wait for a second to load all the data
   setTimeout(loadPlanetsDropdown, 3000); // same thing as above
+  setTimeout(loadVehiclesDropdown, 3000); // same thing as above
+
+  // request({url: 'https://swapi.co/api/species/?page=1'})
+  //   .then(data => {
+  //     // add the responses to the variable
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+
+  //after the content is loaded
+  // modal.style.display = "none";
+  // charCreation.className = "";
 };
 
-window.onunload = function() {
-  console.log('closing the browser');
-  localStorage.setItem('speciesList', JSON.stringify(speciesList));
-  localStorage.setItem('planetsList', JSON.stringify(planestsList));
-};
+// window.onunload = function() {
+//   console.log('closing the browser');
+//   localStorage.setItem('speciesList', JSON.stringify(speciesList));
+//   localStorage.setItem('planetsList', JSON.stringify(planestsList));
+// };
 
 function requestSpecies(pageNumber) { // recursive function so that it is not dependent on hard coding how many pages
   const req = new XMLHttpRequest();
@@ -49,6 +68,43 @@ function requestSpecies(pageNumber) { // recursive function so that it is not de
   event.preventDefault();
 }
 
+let request = obj => {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    // const URLhost = 'https://swapi.co/api/species/?page=' + pageNumber; // sets up to iterate through the pages
+    req.open(obj.method || 'GET', obj.url);
+    if (obj.headers) {
+      Object.keys(obj.headers).forEach(key => {
+        xhr.setRequestHeader(key, obj.headers[key]);
+      });
+    }
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send(obj.body);
+    // req.addEventListener('load',function(){
+    //   if(req.status >= 200 && req.status < 400){
+    //     const response = JSON.parse(req.responseText);
+    //     for (let i = 0; i < response.results.length; i++) { // add each object to the list
+    //       speciesList.push(response.results[i]);
+    //     }
+    //     if (response.next) {
+    //       requestSpecies(pageNumber+1); // recursivly call the function with the new page
+    //     }
+    //   } else {
+    //     console.log('Error in network request: ' + req.statusText);
+    //   }
+    // });
+    // req.send(null);
+    // event.preventDefault();
+  });
+};
+
 function requestPlanets(pageNumber) { // recursive function so that it is not dependent on hard coding how many pages
   const req = new XMLHttpRequest();
   const URLhost = 'https://swapi.co/api/planets/?page=' + pageNumber; // sets up to iterate through the pages
@@ -70,13 +126,34 @@ function requestPlanets(pageNumber) { // recursive function so that it is not de
   event.preventDefault();
 }
 
-function loadSpeciesDropdown() {
-  console.log('organizing the Species');
-  console.log(speciesList);
-  // speciesList.sort(function(a, b){return speciesList[a].name - speciesList[b].name});
-  console.log(speciesList);
+function requestVehicles(pageNumber) { // recursive function so that it is not dependent on hard coding how many pages
+  const req = new XMLHttpRequest();
+  const URLhost = 'https://swapi.co/api/vehicles/?page=' + pageNumber; // sets up to iterate through the pages
+  req.open('GET', URLhost, true);
+  req.addEventListener('load',function(){
+    if(req.status >= 200 && req.status < 400){
+      const response = JSON.parse(req.responseText);
+      for (let i = 0; i < response.results.length; i++) { // add each object to the list
+        vehiclesList.push(response.results[i]);
+      }
+      if (response.next) {
+        requestVehicles(pageNumber+1); // recursivly call the function with the new page
+      }
+    } else {
+      console.log('Error in network request: ' + req.statusText);
+    }
+  });
+  req.send(null);
+  event.preventDefault();
+}
 
-  console.log('loading the dropdown');
+function loadSpeciesDropdown() {
+  // console.log('organizing the Species');
+  // console.log(speciesList);
+  // // speciesList.sort(function(a, b){return speciesList[a].name - speciesList[b].name});
+  // console.log(speciesList);
+
+  console.log('loading the species dropdown');
   for (let i = 0; i < speciesList.length; i++) {
     let speciesName = document.createElement('option');
     speciesName.innerHTML = speciesList[i].name;
@@ -85,11 +162,20 @@ function loadSpeciesDropdown() {
 }
 
 function loadPlanetsDropdown() {
-  console.log('loading the dropdown');
+  console.log('loading the plantes dropdown');
   for (let i = 0; i < planetsList.length; i++) {
     let planetsName = document.createElement('option');
     planetsName.innerHTML = planetsList[i].name;
     document.getElementById('planetsDropdown').add(planetsName);
+  }
+}
+
+function loadVehiclesDropdown() {
+  console.log('loading the vehicles dropdown');
+  for (let i = 0; i < vehiclesList.length; i++) {
+    let vehiclesName = document.createElement('option');
+    vehiclesName.innerHTML = vehiclesList[i].name;
+    document.getElementById('vehiclesDropdown').add(vehiclesName);
   }
 }
 
@@ -100,8 +186,14 @@ document.getElementById('startGame').onclick = function() { // initialize the ga
   const planetsDropdown = document.getElementById('planetsDropdown');
   const species = speciesDropdown.options[speciesDropdown.selectedIndex].text;
   const planet = planetsDropdown.options[planetsDropdown.selectedIndex].text;
-  const myPlayer = character(name, species, planet);
-  console.log(`${myPlayer.getName()} ${myPlayer.getSpecies()} ${myPlayer.getPlanet()}`);
+  const vehicle = vehiclesDropdown.options[vehiclesDropdown.selectedIndex].text;
+  const myPlayer = character(name, species, planet, vehicle);
+  console.log(`
+    Name: ${myPlayer.getName()}
+    Species: ${myPlayer.getSpecies()}
+    Planet: ${myPlayer.getPlanet()}
+    Vehicle: ${myPlayer.getVehicle()}`
+  );
 };
 
 function welcome() {
@@ -109,7 +201,7 @@ function welcome() {
 }
 
 // Global variables
-const character = function (myName, mySpecies, myPlanet) {
+const character = function (myName, mySpecies, myPlanet, myVehicle) {
   let hp = 10;
   let damage = 2;
   let speed = 5;
@@ -117,6 +209,7 @@ const character = function (myName, mySpecies, myPlanet) {
   const name = myName;
   const species = mySpecies;
   const planet = myPlanet;
+  const vehicle = myVehicle;
   return Object.create ({
     getHealth: () => hp,
     getDamage: () => damage,
@@ -125,12 +218,14 @@ const character = function (myName, mySpecies, myPlanet) {
     getName: () => name,
     getSpecies: () => species,
     getPlanet: () => planet,
+    getVehicle: () => vehicle,
 
-    setHealth: (health) => {hp = health},
-    setDamage: (dmg) => {damage = dmg},
-    setSpeed: (speed) => {this.speed = speed},
-    setShield: (shield) => {this.shield = shield}
+    setHealth: (health) => {hp = health;},
+    setDamage: (dmg) => {damage = dmg;},
+    setSpeed: (speed) => {this.speed = speed;},
+    setShield: (shield) => {this.shield = shield;}
   });
 };
 var speciesList = []; // the array to hold all the species
-var planetsList = []; // the array to hold all the species
+var planetsList = []; // the array to hold all the planets
+var vehiclesList = []; // the array to hold all the vehicles
